f139c0dca2646b7f38769fb0dc30040edf2f332c
Aditya Satokar <145588107+AdityaSatokar@users.noreply.github.com>
2024-11-20 12:39:09 +0530

    Update BuyActionWindow.js

--- src/components/BuyActionWindow.js
+++ src/components/BuyActionWindow.js
@@ -14,7 +14,7 @@ const BuyActionWindow = ({ uid }) => {
   const generalContext = useContext(GeneralContext);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("https://zerodha-clone-backend-adityasatokar.vercel.app/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
